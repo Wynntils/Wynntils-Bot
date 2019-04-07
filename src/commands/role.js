2f2cef0800ed3b8045c567b56d586b68dfe693d7
HighCrit <35771251+HighCrit@users.noreply.github.com>
2019-04-07 22:38:04 +0200

    debug

--- src/commands/role.js
+++ src/commands/role.js
@@ -1,7 +1,7 @@
 module.exports = {
     info: {
         name: 'Roles',
-        desc: 'Manage a user\'s roles.\nrole <give|remove> <user> <role>\nrole list',
+        desc: 'Manage a user\'s roles.\n-`role <give|remove> <user> <role>`\n-`role list`',
         help: 'role',
         category: "Moderation",
         uses: [
@@ -25,13 +25,21 @@ module.exports = {
             let manageableRoles = ["Accepted", 538395171881222159, "Muted", 538503043147235348 , "Translator", 563486497374863361];
             if (args.length === 3) {
                 if (manageableRoles.includes(args[2])) {
-                    var user = msg.channel.guild.members.find(member => member.username === args[1]); //By name
-                    if (user == undefined) {
-                        user = msg.channel.guild.members.find(member => member.id === args[1]); //By id
-                        if (user == undefined && msg.mentions.length >= 1) {
-                            user = msg.mentions[0]; //By mention
+                    try {
+                        var user = msg.channel.guild.members.find(member => member.username === args[1]); //By name
+                        if (user == undefined) {
+                            user = msg.channel.guild.members.find(member => member.id === args[1]); //By id
+                            if (user == undefined && msg.mentions.length >= 1) {
+                                user = msg.mentions[0]; //By mention
+                            }
                         }
+                    } catch (error) {
+                      e.embed.fields.push({
+                          name: "Error",
+                          value: "Could not find user"
+                      })  
                     }
+                    
                     if (user == undefined) {
                         e.embed.fields.push({
                             name: "Invalid username/id",
