930d9e6987dc09275d463293d78e152ba3826ddb
HighCrit <35771251+HighCrit@users.noreply.github.com>
2019-04-08 15:34:54 +0200

    At the end of the road

--- src/commands/faq.js
+++ src/commands/faq.js
@@ -9,8 +9,9 @@ module.exports = {
         ]
     },
     execute: (bot, r, msg, args) => {
+        var e;
         if (args.length === 2) {
-            var e = {
+            e = {
                 embed: {
                     title: 'Wynntils FAQ',
                     color: 7531934,
@@ -33,6 +34,7 @@ module.exports = {
                 }
             }
             if (user !== undefined) {
+                var succes = false;
                 switch (args[1].toLowerCase()) {
                     case "wp":
                     case "waypoint":
@@ -40,12 +42,14 @@ module.exports = {
                             name: "Waypoints",
                             value: `<@${user.id}> - Waypoint are currently in development, and, therefore cannot be created in-game as of yet.`
                         });
+                        succes = true;
                         break;
                     case "install":
                         e.embed.fields.push({
                             name: "Installing Wynntils",
                             value: `<@${user.id}> - Here are some guides to help you get started [Wynncraft Forum](https://forums.wynncraft.com/threads/197716/) or [Wynntils' Install Page](https://wynntils.com/install/). Make sure you're installing the recommended version of forge 1.12.2 while setting up Wynntils.`
                         });
+                        succes = true;
                         break;
                     case "capes":
                     case "cape":
@@ -53,6 +57,7 @@ module.exports = {
                             name: "Wynntils Capes",
                             value: `<@${user.id}> - In order to upload a cape you'll need to setup a Wynntils account first. You can do so by going in-game (with Wynntils installed) and using the command \`/token\` this will give you a clickable token. Now think of a password to use for your Wynntils account. You can now log-in at [Wynntils Accounts](https://account.wynntils.com/login.php) with your Minecraft username as username.\n*Note, capes and such have to be upload as a 64x128 image.`
                         });
+                        succes = true;
                         break;
                     case "highlight":
                     case "highlights":
@@ -60,12 +65,14 @@ module.exports = {
                             name: "Item/Player HighLights",
                             value: `<@${user.id}> - Item and player highlights will not be implemented by Wynntils as Wynncraft does not allow for such functionality. If you're using any other mod that provides this feature do so at your own risk - It's a bannable offense.`
                         });
+                        succes = true;
                         break;
                     case "ban":
                         e.embed.fields.push({
                             name: "Is Wynntils Bannable?",
                             value: `<@${user.id}> - You won't we banned for Wynntils. Though, if you get somehow banned for using Wynntils your appeal will be accepted. You can appeal on [Wynncraft's Forum](https://forums.wynncraft.com/) or send a message to JPresent (if it's regarding Wynntils).`
                         });
+                        succes = true;
                         break;
                     case "support":
                     case "crash":
@@ -74,16 +81,17 @@ module.exports = {
                             name: "Crash, Bug or Support",
                             value: `<@${user.id}> - Go into #bot-commands and use \`-new [Optional message]\` this will create a channel where the Support Team will assist you with your troubles.`
                         });
+                        succes = true;
                         break;
                 }
 
-                if (e.embed.fields.length >= 1) {
-                    e.createMessage(e);
+                if (succes) {
+                    msg.channel.createMessage(e);
                 }
             }
         } else if (args.length == 1 && msg.channel.id === "425293785338085387") {
             if (args[0].toLowerCase() === "list" || args[0].toLowerCase() === "l") {
-                var e = {
+                e = {
                     embed: {
                         title: 'Wynntils FAQ',
                         color: 7531934,
@@ -98,8 +106,8 @@ module.exports = {
                     name: "Tag List",
                     value: "\`waypoint\`, \`wp\`, \`install\`, \`capes\`, \`cape\`, \`support\`, \`crash\`, \`bug\`, \`highlights\`, \`highlight\` & \`ban\`"
                 });
-                e.createMessage(e);
+                msg.channel.createMessage(e);
             }
         }
-    }
+    } 
 };
