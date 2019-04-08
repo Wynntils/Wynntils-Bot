ff1813a258176c35c97049c6c716d1629e2929c9
HighCrit <35771251+HighCrit@users.noreply.github.com>
2019-04-08 14:17:27 +0200

    IndexOf not working properly and viva la lowercase!

--- src/commands/role.js
+++ src/commands/role.js
@@ -25,7 +25,7 @@ module.exports = {
             let manageableRoles = ["accepted", "538395171881222159", "muted", "538503043147235348" , "translator", "563486497374863361"];
             if (args.length === 3) {
                 if (manageableRoles.includes(args[2].toLowerCase())) {
-                    var user = msg.channel.guild.members.find(member => member.username.startsWith(args[1])); //By name
+                    var user = msg.channel.guild.members.find(member => member.username.toLowerCase().startsWith(args[1].toLowerCase())); //By name
                     if (user == undefined) {
                         user = msg.channel.guild.members.find(member => member.id === args[1]); //By id
                         if (user == undefined && msg.mentions.length >= 1) {
@@ -57,13 +57,13 @@ module.exports = {
                                     user.addRole(roleId, 10, `Given by ${msg.author.username}#${msg.author.discriminator}`);
                                     e.embed.fields.push({
                                         name: "Role succesfully given",
-                                        value: `${manageableRoles[manageableRoles.indexOf(roleId) - 1]} succesfully given to ${user.user.username}#${user.user.discriminator} by ${msg.author.username}#${msg.author.discriminator}`
+                                        value: `${manageableRoles.indexOf(roleId)} succesfully given to ${user.user.username}#${user.user.discriminator} by ${msg.author.username}#${msg.author.discriminator}`
                                     });
                                 }
                                 break;
                             case "remove":
                             case "r":
-                                if (user.roles.includes(roleId)) {
+                                if (!user.roles.includes(roleId)) {
                                     e.embed.fields.push({
                                         name: "Role not removed",
                                         value: `The user does not posses the role - \`${manageableRoles[manageableRoles.indexOf(roleId) - 1]}\``
@@ -72,7 +72,7 @@ module.exports = {
                                     user.removeRole(roleId, `Removed by ${msg.author.username}#${msg.author.discriminator}`);
                                     e.embed.fields.push({
                                         name:  "Role succesfully removed",
-                                        value: `${manageableRoles[manageableRoles.indexOf(roleId) - 1]} succesfully removed from ${user.user.username}#${user.user.discriminator} by ${msg.author.username}#${msg.author.discriminator}` 
+                                        value: `${manageableRoles.indexOf(roleId)} succesfully removed from ${user.user.username}#${user.user.discriminator} by ${msg.author.username}#${msg.author.discriminator}` 
                                     });
                                 }
                                 break;
