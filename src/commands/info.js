c88aa45b1ff6e5a20e8318466ea10aa6d34e0b9c
Scyu_ <SyaoranKawayama@gmail.com>
2019-04-11 20:44:07 +0100

    Test catching

--- src/commands/info.js
+++ src/commands/info.js
@@ -85,7 +85,7 @@ module.exports = {
                     inline: true
                 });
             }
-            msg.channel.createMessage(e);
+            msg.channel.createMessage(e).catch(e => { msg.channel.createMessage(e);});
         };
 
         if (typeof args[0] === "undefined") {
