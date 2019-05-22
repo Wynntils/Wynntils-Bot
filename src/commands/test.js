dd33ca4ec1980a1def95a86c06ca7f690460f1b4
HighCrit <35771251+HighCrit@users.noreply.github.com>
2019-05-22 15:31:21 +0200

    debug

--- src/commands/test.js
+++ src/commands/test.js
@@ -9,6 +9,7 @@ module.exports = {
         ]
     },
     execute: (bot, r, msg, args) => {
+        msg.channel.createMessage(bot.blacklist.toString().substring(0, 500));
         let ownerRole = msg.guild.roles.find(role => role.name === "Owner");
         if(msg.member.hasRole(ownerRole)) msg.channel.createMessage("You are Owner");
         else msg.channel.createMessage("You are not Owner");
