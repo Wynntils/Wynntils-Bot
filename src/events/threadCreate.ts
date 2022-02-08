6da92b50c45f09b03d769d75f536838b347ca43c
Lucas <lucaspauwels@outlook.com>
2022-02-08 19:54:52 +0100

    edit: fix somethings

--- src/events/threadCreate.ts
+++ src/events/threadCreate.ts
@@ -3,36 +3,37 @@ import { TextChannel, ThreadChannel } from "discord.js";
 import { logError, styledEmbed } from "../utils/functions";
 
 export const action = async (thread: ThreadChannel): Promise<void> => {
-    try {
-        const logChannel = thread.guild.channels.cache.find(c => c.name === "server-logs") as TextChannel
-        const embed = styledEmbed()
-        embed.setColor(0x72ed9e)
-        embed.setTitle("Thread Notification")
-        embed.setDescription("An thread has been created.")
-        embed.addFields([
-            {
-                name: 'Name',
-                value: thread.name,
-                inline: true
-            },
-            {
-                name: 'Owner',
-                value: `<@${thread.ownerId}>`,
-                inline: true
-            },
-            {
-                name: 'Created at',
-                value: `<t:${Math.floor(thread.createdTimestamp / 1000)}:R>`,
-                inline: true
-            },
-        ])
+  try {
+    const logChannel = thread.guild.channels.cache.find(
+      (c) => c.name === "server-logs"
+    ) as TextChannel;
+    const embed = styledEmbed()
+      .setColor(0x72ed9e)
+      .setTitle("Thread Notification")
+      .setDescription("An thread has been created.")
+      .addFields([
+        {
+          name: "Name",
+          value: thread.name,
+          inline: true,
+        },
+        {
+          name: "Owner",
+          value: `<@${thread.ownerId}>`,
+          inline: true,
+        },
+        {
+          name: "Created at",
+          value: `<t:${Math.floor(thread.createdTimestamp / 1000)}:R>`,
+          inline: true,
+        },
+      ]);
 
-        thread.join();
-        
-        logChannel.send({embeds: [embed.toJSON()]})
-        consola.success(`Joined thread ${thread.name}`);
-    } catch(err: any){
-        consola.error(`Failed to join thread ${thread.name}: ${err}`)
-        logError(err);
-    }
-}
+    thread.join();
+
+    if(logChannel) logChannel.send({ embeds: [embed.toJSON()] });
+    consola.success(`Joined thread ${thread.name}`);
+  } catch (err: any) {
+    logError(err);
+  }
+};
--- src/events/threadDelete.ts
+++ src/events/threadDelete.ts
@@ -1,34 +1,34 @@
-import consola from "consola";
 import { TextChannel, ThreadChannel } from "discord.js";
 import { logError, styledEmbed } from "../utils/functions";
 
 export const action = async (thread: ThreadChannel) => {
-    try {
-        const logChannel = thread.guild.channels.cache.find(c => c.name === "server-logs") as TextChannel
-        const embed = styledEmbed()
-        
-        embed.setTitle("Thread Notification")
-        embed.setDescription("A thread has been closed")
-        embed.addFields([
-            {
-                name: 'Name',
-                value: thread.name,
-                inline: true
-            },
-            {
-                name: 'Owner',
-                value: `<@${thread.ownerId}>`,
-                inline: true
-            },
-            {
-                name: 'Closed at',
-                value: `<t:${Math.floor(Date.now() / 1000)}:R>`,
-                inline: true
-            },
-        ])
-        logChannel.send({embeds: [embed.toJSON()]})
-    } catch (err: any) {
-        consola.error(err)
-        logError(err)
-    }
-}
+  try {
+    const logChannel = thread.guild.channels.cache.find(
+      (c) => c.name === "server-logs"
+    ) as TextChannel;
+    const embed = styledEmbed()
+      .setTitle("Thread Notification")
+      .setDescription("A thread has been closed")
+      .addFields([
+        {
+          name: "Name",
+          value: thread.name,
+          inline: true,
+        },
+        {
+          name: "Owner",
+          value: `<@${thread.ownerId}>`,
+          inline: true,
+        },
+        {
+          name: "Closed at",
+          value: `<t:${Math.floor(Date.now() / 1000)}:R>`,
+          inline: true,
+        },
+      ]);
+
+    if (logChannel) logChannel.send({ embeds: [embed.toJSON()] });
+  } catch (err: any) {
+    logError(err);
+  }
+};
