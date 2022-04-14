7ef3675a7f89c94ce21d92fb5fad8151be747e54
DevChromium <chromium@wynntils.com>
2022-04-14 20:42:19 +0200

    feat: join and leave notifications

--- src/events/guildMemberAdd.ts
+++ src/events/guildMemberAdd.ts
@@ -5,12 +5,13 @@ import { Colors } from '../constants/Colors'
 export const action = async (member: GuildMember): Promise<void> => {
 
     const serverLogChannel = member.guild.channels.cache.find(c => c.name === 'server-logs') as TextChannel
+
     try {
         const embed = styledEmbed()
             .setColor(Colors.BLUE)
-            .setTitle('Leave notification')
+            .setTitle('Join notification')
             .setThumbnail(member.user.avatarURL())
-            .setDescription(`<@${member.user.id}> (\`${member.displayName}#${member.user.discriminator}\`) has left the server!`)
+            .setDescription(`<@${member.user.id}> (\`${member.displayName}#${member.user.discriminator})\`) has joined the server!`)
             .addField('Creation date', `${member.user.createdAt}\n<t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`)
 
         serverLogChannel.send({ embeds: [embed.toJSON()] })
