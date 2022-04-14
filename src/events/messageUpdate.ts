a8fce418b6f2718931cc3e710a6a5232aca47725
DevChromium <chromium@wynntils.com>
2022-04-14 17:09:04 +0200

    feat: Message logging

---
+++ src/events/messageUpdate.ts
@@ -0,0 +1,36 @@
+import { Message, TextChannel } from 'discord.js'
+import { logError, styledEmbed } from '../utils/functions'
+import { Colors } from '../constants/Colors'
+
+export const action = async (oldMessage: Message, newMessage: Message): Promise<void> => {
+    try {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const logChannel = oldMessage.guild.channels.cache.find(
+            c => c.name === 'server-logs'
+        ) as TextChannel
+
+        const embed = styledEmbed()
+            .setColor(Colors.YELLOW)
+            .setAuthor({ name: `${oldMessage.author.username}#${oldMessage.author.discriminator} (${oldMessage.author.id})`, iconURL: oldMessage.author.avatarURL() })
+            .setDescription(`Message ${oldMessage.id} edited in <#${oldMessage.channel.id}>`)
+            .addFields([
+                {
+                    name: 'Before',
+                    value: `\`\`\`${oldMessage.content}\`\`\``
+                },
+                {
+                    name: 'After',
+                    value: `\`\`\`${newMessage.content}\`\`\``
+                }
+            ])
+            .setTimestamp()
+
+        if (logChannel)
+            logChannel.send({ embeds: [embed.toJSON()] })
+
+    } catch (err) {
+        logError(err)
+    }
+
+}
